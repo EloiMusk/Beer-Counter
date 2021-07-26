db6126e5dcccb527aafd72ad32162f70ccf44764
Elio Teuber <elioteuber@outlook.com>
2021-07-26 15:24:02 +0200

    management board initialize

--- managementboard/src/app/app.component.ts
+++ managementboard/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import { NFC } from 'nfc-pcsc';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'managementboard';
+  nfc = new NFC
+
 }
