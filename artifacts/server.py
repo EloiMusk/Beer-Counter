6d35db7c57c648eb4cc3d6ad49aeaeab6496cb04
Elio Teuber <elioteuber@outlook.com>
2021-08-09 20:09:47 +0200

    api routes setup

--- artifacts/server.py
+++ artifacts/server.py
@@ -1,11 +1,23 @@
 import flask
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = 0
+app.config["DEBUG"] = 1
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Welcome to the beer-counter api.</h1> <p>I wonder how u got here...</p>"
+
+
+@app.route('/get_uid', methods=['GET'])
+def get_uid():
+    uid = "Get uid here"
+    return uid
+
+@app.route('/set_display', methods=['POST'])
+def set_display(text):
+    state = False
+    # Set display to text and set state true if succeeded
+    return state
 
 app.run()
