dff6dbea21c2fffb952fd31982c415eccd0ce59a
Elio Teuber <elioteuber@outlook.com>
2021-08-10 23:06:03 +0200

    mooooore db functionalities

--- artifacts/dbUtil.py
+++ artifacts/dbUtil.py
@@ -14,9 +14,11 @@ class dbUtil:
             self.cur.execute('''CREATE TABLE beverages
                             (id INTEGER PRIMARY KEY, name text, alc real, price real)''')
             self.cur.execute('''CREATE TABLE logs
-                            (timestamp text, person_id integer, beverage_id integer)''')
+                            (id INTEGER PRIMARY KEY, timestamp text, person_id integer, beverage_id integer)''')
             self.con.commit()
+            return True
         except:
+            self.con.rollback()
             return False
 
     def add_user(self, user):
@@ -24,7 +26,9 @@ class dbUtil:
             self.cur.execute('''INSERT INTO persons VALUES
                              (NULL ,?, ?, ?, ?, ?)''', (user.name, user.avatar, user.uid, user.password, user.role))
             self.con.commit()
+            return True
         except:
+            self.con.rollback()
             return False
 
     def del_user(self, name):
@@ -34,19 +38,57 @@ class dbUtil:
             self.cur.execute('''DELETE FROM persons WHERE name = ?''', [name])
             self.cur.execute('''DELETE FROM logs WHERE person_id = ?''', [id])
             self.con.commit()
+            return True
         except:
+            self.con.rollback()
             return False
+
     def user_exists(self, name):
         try:
             self.cur.execute('''select name from persons where name = ?''', [name])
             req = self.cur.fetchone()[0]
-            print(req)
             if req == name:
-                print(True)
                 return True
             else:
-                print(False)
                 return False
         except:
             return False
+
+    def add_log(self, log):
+        try:
+            self.cur.execute('''insert into logs values
+            (NULL, ?, ?, ?)''', [log.timestamp, log.person_id, log.beverage_id])
+            self.con.commit()
+            return True
+        except:
+            self.con.rollback()
+            return False
+
+    def del_log(self, id):
+        try:
+            self.cur.execute('''DELETE FROM logs where id = ?''', [id])
+            self.con.commit()
+            return True
+        except:
+            self.con.rollback()
+            return False
+
+    def add_beverage(self, beverage):
+        try:
+            self.cur.execute('''insert into beverages values
+            (NULL, ?, ?, ?)''', [beverage.name, beverage.alc, beverage.price])
+            self.con.commit()
+            return True
+        except:
+            self.con.rollback()
+            return False
+    def del_beverage(self, id):
+        try:
+            self.cur.execute('''delete from beverage where id = ?''', [id])
+            self.con.commit()
+            return True
+        except:
+            self.con.rollback()
+            return False
+
 
--- artifacts/test.py
+++ artifacts/test.py
@@ -1,13 +1,16 @@
 from dbUtil import dbUtil
-from dataObjects import user
+from dataObjects import user, log
+from datetime import datetime
 
 db = dbUtil('./../db/beerCounter.db')
 
 # db.setup_beerCounter()
 
 user = user("Person1")
+log = log(datetime.now().isoformat(), 1, 1)
 
 # db.user_exists("Person1")
 # db.add_user(user)
-db.del_user('Person1')
-
+# db.del_user('Person1')
+# db.add_log(log)
+db.del_log(1)
