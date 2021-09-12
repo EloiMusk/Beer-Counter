aef0114bc1ee7aef0a4fd9574dc9372a383b3bb1
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2021-09-12 23:27:16 +0200

    Changed to support new DBUtil-Module

---
+++ artifacts/Server.py
@@ -0,0 +1,106 @@
+from random import random
+import flask
+# from nfcUtil import nfcUtil
+from flask import request, jsonify
+import dataObjects
+from dbUtil import personsDB, logsDB, beveragesDB
+
+#db = dbUtil('./../db/beerCounter.db')
+# nfc = nfcUtil()
+persons = personsDB('./../db/beerCounter.db', 'persons')
+logs = logsDB('./../db/beerCounter.db', 'logs')
+beverages = beveragesDB('./../db/beerCounter.db', 'beverages')
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = 1
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Welcome to the beer-counter api.</h1> <p>I wonder how u got here...</p>"
+
+
+@app.route('/get_uid', methods=['GET'])
+def get_uid():
+    # uid = nfc.get_uid()
+    uid = int(random()*1000000)
+    # print(uid)
+    return jsonify(uid)
+
+
+@app.route('/set_display', methods=['POST'])
+def set_display():
+    state = "False"
+    print(request.json)
+    # TODO Set display to text and set state true if succeeded
+    return jsonify(request.json)
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    msg = "Something went wrong."
+    user = dataObjects.user(**request.json)
+    try:
+        if not persons.exists(user.name) and not user.name is None:
+            persons.add(user)
+            state = "True"
+            return jsonify(state=state)
+        else:
+            state = "False"
+            msg = "User already exists."
+            return jsonify(state=state, msg=msg)
+    except:
+        state = "False"
+        return jsonify(state=state, msg=msg)
+
+@app.route('/delete_user', methods=['DELETE'])
+def delete_user():
+    return True
+
+@app.route('/get_user', methods=['GET'])
+def get_user():
+    return True
+
+@app.route('/add_log', methods=['POST'])
+def add_log():
+    msg = "Something went wrong."
+    log = dataObjects.log(**request.json)
+    try:
+        logs.add(log)
+        state = "True"
+        return jsonify(state=state)
+    except:
+        state = "False"
+        return jsonify(state=state, msg=msg)
+
+@app.route('/del_log', methods=['POST'])
+def del_log():
+    return True
+
+@app.route('/get_log', methods=['GET'])
+def get_log():
+    return True
+
+@app.route('/get_logs', methods=['GET'])
+def get_logs():
+    return True
+
+@app.route('/add_beverage', methods=['POST'])
+def add_beverage():
+    msg = "Something went wrong."
+    beverage = dataObjects.beverage(**request.json)
+    try:
+        if not beverages.exists(beverage.name) and not beverage.name is None:
+            beverages.add(beverage)
+            state = "True"
+            return jsonify(state=state)
+        else:
+            state = "False"
+            msg = "Beverage already exists."
+            return jsonify(state=state, msg=msg)
+    except:
+        state = "False"
+        return jsonify(state=state, msg=msg)
+
+
+app.run()
