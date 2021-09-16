4bf3a1c0e708396426acfff2169a0bef775c82c3
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2021-09-16 11:47:55 +0200

    Update dataObjects.py

--- artifacts/dataObjects.py
+++ artifacts/dataObjects.py
@@ -1,24 +1,45 @@
 
 class user:
     
-    def __init__(self, name, avatar='', uid=None, password=None, role=2):
-        self.name = name
-        self.avatar = avatar
-        self.uid = uid
-        self.password = password
-        self.role = role
+    def __init__(self, name=None, avatar='', uid=None, password=None, role=2):
+        if not name == None and not avatar == '' and not uid == None and not password == None and not role == None:
+            self.name = name
+            self.avatar = avatar
+            self.uid = uid
+            self.password = password
+            self.role = role
+
+        else:
+            if name == None:
+                ValueError(name)
+
+            if avatar == ''
 
 class beverage:
     
-    def __init__(self, name, alc=None, price=None):
+    def __init__(self, name=None, alc=None, price=None):
         self.name = name
         self.alc = alc
         self.price = price
 
 class log:
     
-    def __init__(self, timestamp, person_id, beverage_id):
+    def __init__(self, timestamp=None, person_id=None, beverage_id=None):
         self.timestamp = timestamp
         self.person_id = person_id
         self.beverage_id = beverage_id
-        
+
+"""
+class nfcTag:
+    def __init__(self, uid=None, person_id=None):
+        if not uid == None and not person_id = None:
+            self.uid = uid
+            self.person_id = person_id
+
+        else:
+            if uid == None:
+                ValueError(uid)
+
+            if person_id == None:
+                ValueError(person_id)
+"""
